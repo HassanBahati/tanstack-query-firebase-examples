70f05ce0db797349893b237194a893f05d743450
HassanBahati <mukisabahati@gmail.com>
2025-03-04 20:56:48 +0300

    feat: setup providers

---
+++ angular/firebase-dataconnect-movies-app/dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'firebase-dataconnect-movies-app',
+  location: 'us-central1'
+};
+
--- angular/firebase-dataconnect-movies-app/src/app/app.routes.ts
+++ angular/firebase-dataconnect-movies-app/src/app/app.routes.ts
@@ -1,3 +1,29 @@
+import { ApplicationConfig } from '@angular/core';
 import { Routes } from '@angular/router';
+import { connectorConfig } from '@firebasegen/default-connector';
 
+import {
+  provideDataConnect,
+  getDataConnect,
+  connectDataConnectEmulator,
+} from '@angular/fire/data-connect';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import {
+  provideTanStackQuery,
+  QueryClient,
+} from '@tanstack/angular-query-experimental';
+import { environment } from '../environments/environment';
 export const routes: Routes = [];
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideTanStackQuery(new QueryClient()),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideDataConnect(() => {
+      const dc = getDataConnect(connectorConfig);
+      // Connect to the Data Connect emulator
+      connectDataConnectEmulator(dc, 'localhost', 9399);
+      return dc;
+    }),
+  ],
+};
